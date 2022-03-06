14b1823079f62d8d0a980c3a1c72ed5efbfda1b1
Rafael Sanzio <rafaelsanzio16@gmail.com>
2022-03-06 17:00:33 -0300

    preenchendo vetor 2 c#

---
+++ C#/URI/117-preenchimentoVetorll.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/*
+Faça um programa que leia um valor T e preencha um vetor N[1000] com a sequência de valores de 0 até T-1 repetidas vezes, conforme exemplo abaixo. Imprima o vetor N.
+
+Entrada
+A entrada contém um valor inteiro T (2 ≤ T ≤ 50).
+
+Saída
+Para cada posição do vetor, escreva "N[i] = x", onde i é a posição do vetor e x é o valor armazenado naquela posição.
+*/
+
+
+namespace URI
+{
+    public class _117_preenchimentoVetorll
+    {
+        public static void Main(string[] agrs)
+        {
+            int[] N = new int[10];
+            int T = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < N.Length; i++)
+            {
+                N[i] = i % T;
+                Console.WriteLine("N[{0}] = {1}", i, N[i]);
+            }
+        }
+    }
+}
